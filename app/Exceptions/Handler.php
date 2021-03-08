4c99d042a3baebd3d602fa5445406fbdb0620f47
raihan <ryihan55@gmail.com>
2021-03-09 01:23:09 +0600

    Update Handler.php

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -7,6 +7,26 @@ use Throwable;
 
 class Handler extends ExceptionHandler
 {
+    protected $dontReport = [
+        //
+    ];
+
+    /**
+     * A list of the inputs that are never flashed for validation exceptions.
+     *
+     * @var array
+     */
+    protected $dontFlash = [
+        'current_password',
+        'password',
+        'password_confirmation',
+    ];
+
+    /**
+     * Register the exception handling callbacks for the application.
+     *
+     * @return void
+     */
     public function register()
     {
         $this->reportable(function (Throwable $e) {
