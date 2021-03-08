93b3bd1c54ba9a78da4edd7399c4646ecabad72e
raihan <ryihan55@gmail.com>
2021-03-09 01:24:48 +0600

    Create RedirectIfAuthenticated.php

---
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Providers\RouteServiceProvider;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class RedirectIfAuthenticated
+{
+}
