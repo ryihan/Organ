266228c781c32ab4c059cb2fefa00b15b036e654
raihan <ryihan55@gmail.com>
2021-03-09 01:22:47 +0600

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -6,6 +6,12 @@ use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
+{
+    /**
+     * The Artisan commands provided by your application.
+     *
+     * @var array
+     */
     protected $commands = [
         //
     ];
