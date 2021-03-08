56ec1deaf114568377ffe26c08992e9f74316c95
raihan <ryihan55@gmail.com>
2021-03-09 01:26:59 +0600

    Update BroadcastServiceProvider.php

--- app/Providers/BroadcastServiceProvider.php
+++ app/Providers/BroadcastServiceProvider.php
@@ -7,4 +7,15 @@ use Illuminate\Support\ServiceProvider;
 
 class BroadcastServiceProvider extends ServiceProvider
 {
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Broadcast::routes();
+
+        require base_path('routes/channels.php');
+    }
 }
