1e4fbeb896063425b760409b7a50474d25708712
raihan <ryihan55@gmail.com>
2021-03-09 01:26:02 +0600

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,6 +10,23 @@ use Illuminate\Notifications\Notifiable;
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
     protected $hidden = [
         'password',
         'remember_token',
