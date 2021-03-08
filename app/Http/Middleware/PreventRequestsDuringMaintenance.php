246f19f1a6e0e78a32bdb2d4dfafcaf4c3a2db81
raihan <ryihan55@gmail.com>
2021-03-09 01:24:34 +0600

    Create PreventRequestsDuringMaintenance.php

---
+++ app/Http/Middleware/PreventRequestsDuringMaintenance.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Foundation\Http\Middleware\PreventRequestsDuringMaintenance as Middleware;
+
+class PreventRequestsDuringMaintenance extends Middleware
+{
+}
